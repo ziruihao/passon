391778981e53f1d2df898922eabadb5eec1dd8a9
Gillian Yue <gillianyue1999@hotmail.com>
2019-05-19 02:25:24 -0400

    messaging front end

---
+++ components/Chat.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
+import io from 'socket.io-client';
+
+class Chat extends React.Component {
+    static navigationOptions = ({ navigation }) => ({
+      title: (navigation.state.params || {}).name || 'Chat!',
+    });
+
+    constructor(props) {
+      super(props);
+      const room = 'abc123'; // would be other user's id from this.props
+
+      // Creating the socket-client instance will automatically connect to the server.
+      const socket = io('http://localhost:3000');
+      socket.on('connect', () => {
+      // Connected, let's sign-up for to receive messages for this room
+        socket.emit('room', room);
+      });
+
+      // socket.on('message', (data) => {
+      //   console.log('Incoming message:', data);
+      // });
+      socket.on('received', (message) => {
+        console.log('Incoming message:', message);
+        const oldMessages = () => this.state.messages;
+        this.setState({ messages: oldMessages.concat(message) });
+      });
+      this.state = {
+        socket,
+        messages: [],
+      };
+      this.sendMessage = this.sendMessage.bind(this);
+    }
+
+    componentWillMount() {
+      this.setState({
+        messages: [
+          {
+            _id: 1,
+            text: 'Hello developer',
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: 'React Native',
+              avatar: 'https://placeimg.com/140/140/any',
+            },
+          },
+        ],
+      });
+    }
+
+    sendMessage(messages = []) {
+      console.log(`messages sent: ${JSON.stringify(messages)}`);
+      const message = {
+        body: {
+          text: messages.text,
+          createdAt: messages.createdAt,
+          userId: messages.user,
+          chatId: 445566,
+        },
+      };
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }));
+      this.state.socket.emit('message', message);
+    }
+
+    render() {
+      return (
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.sendMessage(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+      );
+    }
+}
+export default Chat;
