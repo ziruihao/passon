b63418dfbbe6f453b4be7e46d4c851e576936527
John Sullivan <sulljohn12@gmail.com>
2019-05-20 13:18:55 -0400

    Updating the controllers; still need to finish the delete one for skills ...

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -21,6 +21,7 @@ export const signin = (req, res) => {
     });
 };
 
+// Signs up a user and generates a token for them
 export const signup = (req, res, next) => {
   const { firstName } = req.body;
   const { lastName } = req.body;
@@ -60,28 +61,28 @@ export const signup = (req, res, next) => {
   return next;
 };
 
-
+// Gets all users (modified so that it does not return any password information; add other fields to return as needed
 export const getUsers = (req, res) => { // TODO: return based on searched skill
   User.find({}).then((result) => {
-    // const out = [];
+    const out = [];
 
-    // result.forEach((elem) => {
-    //   out.push({
-    //     firstName: elem.firstName,
-    //     lastName: elem.lastName,
-    //     email: elem.email,
-    //   });
-    // });
+    result.forEach((elem) => {
+      out.push({
+        firstName: elem.firstName,
+        lastName: elem.lastName,
+        email: elem.email,
+      });
+    });
 
     // DO NOT WANT TO DIRECTLY SEND RESULT, IT WOULD CONTAIN PASSWORD INFO
 
-    // res.send(out);
-    res.send(result);
+    res.send(out);
   }).catch((error) => {
     res.status(500).json({ error });
   });
 };
 
+// Gets a user (also modified not to return too much information)
 export const getUser = (req, res) => {
   User.findById(req.params.id)
     .then((result) => {
@@ -98,6 +99,7 @@ export const getUser = (req, res) => {
     });
 };
 
+// Deletes an entire user
 export const deleteUser = (req, res) => {
   // res.send('delete a post here');
   User.findByIdAndDelete(req.params.id)
@@ -109,6 +111,7 @@ export const deleteUser = (req, res) => {
     });
 };
 
+// Update user parameters (just use this for username, email, simple fields (NOT skills)
 export const updateUser = (req, res) => {
   User.findByIdAndUpdate(req.params.id, req.body)
     .then(() => {
@@ -125,6 +128,7 @@ export const updateUser = (req, res) => {
     });
 };
 
+// Add a skill for a user to learn
 export const addSkill = (req, res) => {
   User.findById(req.params.id)
     .then((result) => {
@@ -165,15 +169,78 @@ export const addSkill = (req, res) => {
     });
 };
 
+// Add a skill that a user can teach
+export const addTeach = (req, res) => {
+  // Only push to that user's teach library
+  User.findById(req.params.id)
+    .then((result) => {
+      result.teach.push({
+        title: req.body.title,
+        years: req.body.years,
+        description: req.body.description,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
+
+// Delete a skill a user wants to learn
 export const delSkill = (req, res) => {
 
 };
 
+// Delete a skill a user wants to teach
+export const delTeach = (req, res) => {
+
+};
+
+// Update a skill for a user
+export const updateSkill = (req, res) => {
+  User.findById(req.params.id)
+    .then((result) => {
+      result.learn.forEach((element) => {
+        if (element.title === req.params.title) {
+          element.description = req.params.description;
+        }
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
+
+// Update a skill that a user wants to teach
+export const updateTeach = (req, res) => {
+  User.findById(req.params.id)
+    .then((result) => {
+      result.teach.forEach((element) => {
+        if (element.title === req.params.title) {
+          element.description = req.params.description;
+        }
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
+
+// Get information on a specific skill (the users associated with it)
 export const getSkill = (req, res) => {
-
+  Skill.findById(req.params.id)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
 };
 
+// Get information on all skills (and all users associated with them)
 export const getSkills = (req, res) => {
-
+  Skill.find({}).then((result) => {
+    res.send(result);
+  }).catch((error) => {
+    res.status(500).json({ error });
+  });
 };
-
