e2cad793702a3482a77990e3ce1f4f402d49cca7
Gillian Yue <gillianyue1999@hotmail.com>
2019-05-21 16:06:07 -0400

    create Chat properly storing objectIDs of users

--- src/controllers/message_controller.js
+++ src/controllers/message_controller.js
@@ -23,27 +23,27 @@ export const saveMessage = (req, res) => {
 //
 
 export const createChat = (req, res) => {
-  const chat = new Chat(); let user1 = 'default';
+  const chat = new Chat();
   User.find({ email: req.body.email.toLowerCase() }).then((result) => {
-    user1 = result;
-    console.log(`user1: ${JSON.stringify(result)}`);
-  });
-  chat.userId = [user1, req.user];
-  // first is the other user, second is self
-  // second should really be req.user since it's in token in the header
-  // right now the react-version local storage is not figured out so
-  // we're arbitrarily passing in a user id for self
-  chat.messages = req.body.messages;
+    console.log(`other user in chat: ${JSON.stringify(result[0])}`);
+    console.log(`self: ${req.user}`);
+    chat.userId = [result[0], req.user];
+    // first is the other user, second is self
+    // second should really be req.user since it's in token in the header
+    // right now the react-version local storage is not figured out so
+    // we're arbitrarily passing in a user id for self
+    chat.messages = req.body.messages;
 
-  chat.save()
-    .then(() => {
-      res.json({ message: 'Chat created!' });
-      console.log('chat created! ');
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-      console.log('chat create failed!');
-    });
+    chat.save()
+      .then(() => {
+        res.json({ message: 'Chat created!' });
+        console.log('chat created! ');
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+        console.log('chat create failed!');
+      });
+  });
 };
 
 export const getChats = (req, res) => {
