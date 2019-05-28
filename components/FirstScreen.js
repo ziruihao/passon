94afa23aeb0f03a8814bac213c98dff1dc625565
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-05-28 01:03:41 -0400

    added auto-sign in for testing

--- components/FirstScreen.js
+++ components/FirstScreen.js
@@ -1,11 +1,15 @@
 /* eslint-disable global-require */
 import React from 'react';
+import { connect } from 'react-redux';
+
 import {
   StyleSheet, View, Button, Text, ImageBackground,
 } from 'react-native';
 import {
   colors, fonts, padding, dimensions,
 } from '../styles/base';
+import { signinUser } from '../actions';
+
 
 const styles = StyleSheet.create({
   container: {
@@ -33,12 +37,20 @@ const styles = StyleSheet.create({
   },
 });
 
+const authTestUser = (props) => {
+  props.signinUser({
+    email: 'Passon',
+    password: 'password',
+  }, props.navigation);
+};
+
 const FirstScreen = (props) => {
   return (
     <View style={styles.container}>
       <ImageBackground source={require('../assets/background.png')} style={{ width: '100%', height: '100%' }}>
         <View style={styles.content}>
           <Text style={styles.title}>PassOn</Text>
+          <View style={styles.button}><Button color={colors.posButton} onPress={() => { authTestUser(props); }} title="Test User: Passon" /></View>
           <View style={styles.button}><Button color={colors.posButton} onPress={() => { props.navigation.navigate('SignIn'); }} title="Sign In" /></View>
           <View style={styles.button}><Button color={colors.posButton} onPress={() => { props.navigation.navigate('SignUp'); }} title="Sign Up" /></View>
         </View>
@@ -47,4 +59,4 @@ const FirstScreen = (props) => {
   );
 };
 
-export default FirstScreen;
+export default connect(null, { signinUser })(FirstScreen);
