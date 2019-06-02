b73b94ecb4ba54f359ed6bdb4f9264a54cf5a896
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-06-02 14:13:46 -0400

    built s3 wrapper

--- containers/Auth/Signup.js
+++ containers/Auth/Signup.js
@@ -1,10 +1,12 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable import/no-duplicates */
 /* eslint-disable global-require */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
-  StyleSheet, Text, View, Button, TextInput, ImageBackground, TouchableOpacity,
+  StyleSheet, Text, View, Button, TextInput, ImageBackground, TouchableOpacity, Image,
 } from 'react-native';
-import { Font } from 'expo';
+import Expo from 'expo';
 import { signupUser } from '../../actions';
 import {
   colors, fonts, padding, dimensions,
@@ -77,17 +79,44 @@ class SignUp extends Component {
     super(props);
 
     this.state = {
+      choosingImage: false,
       firstName: '',
       lastName: '',
       email: '',
       password: '',
       university: '',
+      profileImage: {},
       errorFirstName: false,
       errorLastName: false,
       errorEmail: false,
       errorPassword: false,
       errorUniversity: false,
     };
+  }
+
+  pickProfileImage = async () => {
+    const Permissions = Expo.Permissions;
+    // const { Permissions } = Expo;
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status.status === 'granted') {
+      const options = {
+        mediaTypes: 'Images',
+      };
+      // const pickedImage = await ImagePicker.launchImageLibraryAsync(options);
+      // this.setState({ profileImage: pickedImage });
+      // uploadImage(this.state.file).then((url) => {
+      //   this.setState(prevState => ({
+      //     input: Object.assign({}, prevState.input, { cover_url: url }),
+      //   }));
+      //   // use url for content_url and
+      //   // either run your createPost actionCreator
+      //   // or your updatePost actionCreator
+      //   this.props.makePost(inputModified, this.props.history);
+      // }).catch((error) => {
+      // });
+    } else {
+      throw new Error('Location permission not granted');
+    }
   }
 
   signUp = async () => { // Check that there are no bad or empty values that the user is attempting to signup
@@ -132,74 +161,100 @@ class SignUp extends Component {
   };
 
   render() {
-    return (
-      <View style={styles.container}>
-        <ImageBackground source={require('../../assets/background.png')} style={{ width: '100%', height: '100%' }}>
-          <View style={styles.between}>
-            <View style={styles.content}>
-              <View style={styles.titleContainer}>
-                <Text style={styles.title}>Create an Account</Text>
+    if (!this.state.choosingImage) {
+      return (
+        <View style={styles.container}>
+          <ImageBackground source={require('../../assets/background.png')} style={{ width: dimensions.fullWidth, height: dimensions.fullHeight }}>
+            <View style={styles.between}>
+              <View style={styles.content}>
+                <View style={styles.titleContainer}>
+                  <Text style={styles.title}>Create an Account</Text>
+                </View>
+                { this.state.errorFirstName === true ? (
+                  <Text style={styles.errorText}>
+                    Please enter first name to proceed.
+                  </Text>
+                ) : null }
+                <TextInput
+                  style={styles.input}
+                  placeholder="First Name"
+                  onChangeText={(text) => { this.setState({ firstName: text }); }}
+                />
+                { this.state.errorLastName === true ? (
+                  <Text style={styles.errorText}>
+                 Please enter last name to proceed.
+                  </Text>
+                ) : null }
+                <TextInput
+                  style={styles.input}
+                  placeholder="Last Name"
+                  onChangeText={(text) => { this.setState({ lastName: text }); }}
+                />
+                { this.state.errorEmail === true ? (
+                  <Text style={styles.errorText}>
+                    Please enter email to proceed.
+                  </Text>
+                ) : null }
+                <TextInput
+                  style={styles.input}
+                  placeholder="Email"
+                  onChangeText={(text) => { this.setState({ email: text }); }}
+                />
+                { this.state.errorPassword === true ? (
+                  <Text style={styles.errorText}>
+                Please enter password to proceed.
+                  </Text>
+                ) : null }
+                <TextInput
+                  style={styles.input}
+                  placeholder="Password"
+                  secureTextEntry
+                  onChangeText={(text) => { this.setState({ password: text }); }}
+                />
+                { this.state.errorUniversity === true ? (
+                  <Text style={styles.errorText}>
+                Please enter university to proceed.
+                  </Text>
+                ) : null }
+                <TextInput
+                  style={styles.input}
+                  placeholder="University"
+                  onChangeText={(text) => { this.setState({ university: text }); }}
+                />
+                {/* <TouchableOpacity style={styles.button} onPress={() => { this.pickProfileImage(); }}>
+                  <Text style={styles.buttonText}>Pick Profile Image</Text>
+                </TouchableOpacity> */}
+                <TouchableOpacity style={styles.button} onPress={this.signUp}>
+                  <Text style={styles.buttonText}>Next</Text>
+                </TouchableOpacity>
+                <Image source={{ uri: this.state.profileImage.uri }} style={{ width: this.state.profileImage.width, height: this.state.profileImage.height }} />
               </View>
-              { this.state.errorFirstName === true ? (
-                <Text style={styles.errorText}>
-                  Please enter first name to proceed.
-                </Text>
-              ) : null }
-              <TextInput
-                style={styles.input}
-                placeholder="First Name"
-                onChangeText={(text) => { this.setState({ firstName: text }); }}
-              />
-              { this.state.errorLastName === true ? (
-                <Text style={styles.errorText}>
-               Please enter last name to proceed.
-                </Text>
-              ) : null }
-              <TextInput
-                style={styles.input}
-                placeholder="Last Name"
-                onChangeText={(text) => { this.setState({ lastName: text }); }}
-              />
-              { this.state.errorEmail === true ? (
-                <Text style={styles.errorText}>
-                  Please enter email to proceed.
-                </Text>
-              ) : null }
-              <TextInput
-                style={styles.input}
-                placeholder="Email"
-                onChangeText={(text) => { this.setState({ email: text }); }}
-              />
-              { this.state.errorPassword === true ? (
-                <Text style={styles.errorText}>
-              Please enter password to proceed.
-                </Text>
-              ) : null }
-              <TextInput
-                style={styles.input}
-                placeholder="Password"
-                secureTextEntry
-                onChangeText={(text) => { this.setState({ password: text }); }}
-              />
-              { this.state.errorUniversity === true ? (
-                <Text style={styles.errorText}>
-              Please enter university to proceed.
-                </Text>
-              ) : null }
-              <TextInput
-                style={styles.input}
-                placeholder="University"
-                onChangeText={(text) => { this.setState({ university: text }); }}
-              />
-              <TouchableOpacity style={styles.button} onPress={() => { this.signUp(); }}>
-                <Text style={styles.buttonText}>Sign Up</Text>
-              </TouchableOpacity>
+              <View style={styles.smallText}><Button color={colors.white} onPress={() => { this.props.navigation.navigate('SignIn'); }} title="I already have an account." /></View>
             </View>
-            <View style={styles.smallText}><Button color={colors.white} onPress={() => { this.props.navigation.navigate('SignIn'); }} title="I already have an account." /></View>
-          </View>
-        </ImageBackground>
-      </View>
-    );
+          </ImageBackground>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <ImageBackground source={require('../../assets/background.png')} style={{ width: dimensions.fullWidth, height: dimensions.fullHeight }}>
+            <View style={styles.between}>
+              <View style={styles.content}>
+                <View style={styles.titleContainer}>
+                  <Text style={styles.title}>Pick Profile Image</Text>
+                  <TouchableOpacity style={styles.button} onPress={this.pickProfileImage}>
+                    <Text style={styles.buttonText}>Choose</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.button} onPress={this.signUp}>
+                    <Text style={styles.buttonText}>Skip</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </ImageBackground>
+        </View>
+      );
+    }
   }
 }
 
